7fb473b2a032e21e193150e20be5d3793219ac44
Luan <luancardoso678910@gmail.com>
2026-04-02 22:14:37 -0300

    at

---
+++ app/src/main/java/app/vercel/app_music_eight/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.app_music_eight.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
